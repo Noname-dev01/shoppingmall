3f1e0a10aa19201fb27a9b8e7975bc5204332e13
noname_dev <noname_dev@naver.com>
2021-12-16 17:12:14 +0900

    회원가입 js 및 회원가입 컨트롤러 추가

--- src/main/java/portfolio/shoppingmall/web/member/MemberController.java
+++ src/main/java/portfolio/shoppingmall/web/member/MemberController.java
@@ -2,14 +2,31 @@ package portfolio.shoppingmall.web.member;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import portfolio.shoppingmall.domain.member.Member;
+import portfolio.shoppingmall.domain.member.MemberRepository;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/members")
 public class MemberController {
 
+    private final MemberRepository memberRepository;
+
     @GetMapping("/join")
-    public String joinForm() {return "members/joinForm";}
+    public String joinForm(@ModelAttribute("member") Member member) {return "members/joinForm";}
+
+    @PostMapping("/join")
+    public String save(@Validated @ModelAttribute Member member, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "members/joinForm";
+        }
+        memberRepository.save(member);
+        return "redirect:/";
+    }
 }
